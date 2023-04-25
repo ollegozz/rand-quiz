c9bcbe6ef6df1222f657b222ae9b5c41b5407c37
Олег <OllegOzz@yandex.ru>
2023-04-25 15:15:11 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/header/header';
+import QuizRandPage from './components/page/quizRandPage';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div>
+      <Header />
+      <QuizRandPage />
+      <Footer />
+
+   </div>    
+  )
 }
 
 export default App;
