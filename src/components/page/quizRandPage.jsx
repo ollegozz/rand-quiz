9bedc5e9f8f1432e0b7232db51875c2269ffd688
Олег <OllegOzz@yandex.ru>
2023-04-28 00:04:58 +0300

    quizRandPage-style

--- src/components/page/quizRandPage.jsx
+++ src/components/page/quizRandPage.jsx
@@ -2,57 +2,18 @@ import React from 'react'
 
 export default function QuizRandPage() {
   return (
-    <div className='container mx-auto'>QuizRandPage
-      <div className='flex flex-row text-center'>
-        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
-          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
+    <div className='container mx-auto max-w-8xl'>
+      <h2 className='font-bold text-xl text-center py-8'>QuizRandPage</h2>
+      <div className='flex flex-row text-center flex-wrap justify-center'>
+
+        <div className='basis-1/4 py-2 px-2 border-2 rounded-[12px] m-1 max-w-xs'>
+          <div className='font-bold text-xl p-2'>Lorem ipsum dolor sit amet.</div>
           <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
-          <div className='p-5'>
+          <div className='pt-5 pb-2'>
             <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
           </div>
         </div>
-        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
-          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
-          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
-          <div className='p-5'>
-            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
-          </div>
-        </div>
-        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
-          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
-          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
-          <div className='p-5'>
-            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
-          </div>
-        </div>
-        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
-          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
-          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
-          <div className='p-5'>
-            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
-          </div>
-        </div>
-        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
-          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
-          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
-          <div className='p-5'>
-            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
-          </div>
-        </div>
-        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
-          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
-          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
-          <div className='p-5'>
-            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
-          </div>
-        </div>
-        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
-          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
-          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
-          <div className='p-5'>
-            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
-          </div>
-        </div>
+       
 
       </div>
     </div>
