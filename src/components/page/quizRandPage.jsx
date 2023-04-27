3ed599e34e05b1439013afe9780631cf5ca183c6
Олег <OllegOzz@yandex.ru>
2023-04-27 23:50:23 +0300

    quiz-maket

--- src/components/page/quizRandPage.jsx
+++ src/components/page/quizRandPage.jsx
@@ -2,6 +2,59 @@ import React from 'react'
 
 export default function QuizRandPage() {
   return (
-      <div>QuizRandPage</div>
+    <div className='container mx-auto'>QuizRandPage
+      <div className='flex flex-row text-center'>
+        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
+          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
+          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
+          <div className='p-5'>
+            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
+          </div>
+        </div>
+        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
+          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
+          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
+          <div className='p-5'>
+            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
+          </div>
+        </div>
+        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
+          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
+          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
+          <div className='p-5'>
+            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
+          </div>
+        </div>
+        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
+          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
+          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
+          <div className='p-5'>
+            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
+          </div>
+        </div>
+        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
+          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
+          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
+          <div className='p-5'>
+            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
+          </div>
+        </div>
+        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
+          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
+          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
+          <div className='p-5'>
+            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
+          </div>
+        </div>
+        <div className='basis-1/4 py-2 px-2 m-5 border-2 rounded-[12px] '>
+          <div className='font-bold text-xl p-2'>TitleTitleTitleTitleTitle</div>
+          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
+          <div className='p-5'>
+            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
