4da8ed5b5a481b64a2decfeec3b565710a204ddb
Олег <OllegOzz@yandex.ru>
2023-05-03 16:00:15 +0300

    mock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import React, {useState} from 'react';
 import Header from './components/header/header';
 import QuizRandPage from './components/page/quizRandPage';
 import Footer from './components/footer/footer';
+import mock from './mock.json'
 
 function App() {
+  const [quiz, setQuiz] = useState(mock)
+
   return (
     <div>
       <Header />
-      <QuizRandPage />
+      <QuizRandPage quiz={quiz} setQuiz={setQuiz}/>
       <Footer />
 
    </div>    
--- src/components/page/quizRandPage.jsx
+++ src/components/page/quizRandPage.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 
-export default function QuizRandPage() {
+
+const QuizRandPage = (props) => {
+
+  const { quiz, setQuiz } = props
+
   return (
     <div className='container mx-auto max-w-8xl'>
       <h2 className='font-bold text-xl text-center py-8'>QuizRandPage</h2>
       <div className='flex flex-row text-center flex-wrap justify-center'>
 
-        <div className='basis-1/4 py-2 px-2 border-2 rounded-[12px] m-1 max-w-xs'>
-          <div className='font-bold text-xl p-2'>Lorem ipsum dolor sit amet.</div>
-          <div className='p-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, maxime!</div>
-          <div className='pt-5 pb-2'>
-            <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
+        {quiz.map((item) => (
+          <div className='basis-1/4 py-2 px-2 border-2 rounded-[12px] m-1 max-w-xs' key={item.id}>
+            <div className='font-bold text-xl p-2'>{item.title}</div>
+            <div className='p-2'>{item.question}</div>
+            <div className='pt-5 pb-2'>
+              <button className='border-2 rounded-[12px] px-8  hover:bg-blue-400 hover:text-white text-xl font-bold'>Open</button>
+            </div>
           </div>
-        </div>
-       
+        ))}
 
       </div>
     </div>
   )
 }
+
+export default QuizRandPage
