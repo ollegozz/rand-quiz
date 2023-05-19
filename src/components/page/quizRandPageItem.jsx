d7ad80b46694434811549fb00e4ae51ba80d9616
Олег <OllegOzz@yandex.ru>
2023-05-19 13:59:31 +0300

    1

--- src/components/page/quizRandPageItem.jsx
+++ src/components/page/quizRandPageItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const QuizRandPageItem = (props) => {
@@ -24,7 +25,9 @@ const QuizRandPageItem = (props) => {
             <div>
               <button
                 className='m-10 p-3 px-3 border-2 rounded-full border-blue-500 hover:bg-blue-400 hover:text-white'
-                onClick={getAnswer}>Скрыть ответ</button>
+                onClick={getAnswer}>
+                  <Link to={`/quiz/`}>На страницу загадок</Link>
+                  </button>
               <p>{item.answer}</p>
               <div className=' justify-center flex'>
                 <img
